05bfcdb213227ec6fd89f5e0a39bf0dc0c269447
LloydDon <lloyddon@hotmail.com>
2020-03-19 14:42:02 +0800

    Get user info through accessToken

---
+++ src/main/java/com/nova/nova/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package com.nova.nova.controller;
+
+import com.nova.nova.dto.AccessTokenDTO;
+import com.nova.nova.dto.GithubUser;
+import com.nova.nova.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("5e5b5a70a6c5aba90c17");
+        accessTokenDTO.setClient_secret("0db14ecc10427628a6af03e4caa7d06533016d34");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
