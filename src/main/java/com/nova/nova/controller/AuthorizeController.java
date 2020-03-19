935556c06ed2260f962bc845168ab3a6408d38cc
LloydDon <lloyddon@hotmail.com>
2020-03-19 17:30:29 +0800

    set properties

--- src/main/java/com/nova/nova/controller/AuthorizeController.java
+++ src/main/java/com/nova/nova/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.nova.nova.dto.AccessTokenDTO;
 import com.nova.nova.dto.GithubUser;
 import com.nova.nova.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,19 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("5e5b5a70a6c5aba90c17");
-        accessTokenDTO.setClient_secret("0db14ecc10427628a6af03e4caa7d06533016d34");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        System.out.println(user.getId());
         return "index";
     }
 }
